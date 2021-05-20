52c6642292a1559354a76831db1cd02f30293353
YanivKolesnik <84464310+YanivKolesnik@users.noreply.github.com>
2021-05-20 20:27:27 +0300

    commit 2

--- main.js
+++ main.js
@@ -1,49 +1,73 @@
 'use strict'
-const mines = '💣'
+const MINE = '💣'
+const FLAG = '🏁'
+
+var gBoardMatrix;
+
 var gBoard = {
-    minesAroundCount: 4,
-    isShown: true,
+    minesAroundCount: 0,
+    isShown: false,
     isMine: false,
-    isMarked: true
+    isMarked: false
 }
 
 var gLevel = {
-    SIZE: 4,
-    MINES: 2
+    size: 4,
+    mines: 2,
+    flags: 2,
+    lives: 1
 }
+
 var gGame = {
     isOn: false,
     shownCount: 0,
     markedCount: 0,
-    secsPassed: 0
+    secsPassed: 0,
+    firstMove: true
 }
+
+var gelSmily = document.querySelector('.smiley');
+var gelLives = document.querySelector('.lives');
 
 function initGame() {
-    var board = createBoard();
-    renderBoard(board)
+    gGame.isOn = true;
+    gelSmily.innerText = '😀';
+    gelLives.innerText = 'Left Lives: ' + gLevel.lives;
+    createBoard();
+    renderBoard(gBoardMatrix);
 }
 
-function createBoard(board) {
-    var board = createMat(gBoard, 4, 4);
-    console.table(board)
-    return board
-}
-
-
-function setMinesCount(board) {
+function createBoard() {
+    gBoardMatrix = createMat(gBoard, gLevel.size, gLevel.size);
+    console.table(gBoardMatrix)
 
 }
 
 function renderBoard(board) {
     var strHTML = '';
     for (var i = 0; i < board.length; i++) {
-        strHTML += '<tr>\n';
+        strHTML += '<tr>';
         for (var j = 0; j < board[0].length; j++) {
             var currCell = board[i][j];
-            var className = `cell-${i}-${j}`
-            strHTML += '\t<td class="cell">';
+
+            strHTML += `<td onclick="cellClicked(${i}, ${j}) "`;
+            if (!currCell.isShown) {
+                strHTML += currCell.isMarked ? ` class="hidden">${FLAG}</td>` : ` class="hidden"></td>`;
+            }
+            else {
+                if (currCell.isMine && currCell.isMarked) {
+                    strHTML += ` class="boom">${FLAG}</td>`;
+                }
+                else if (currCell.isMine && !currCell.isMarked)
+                    strHTML += ` class="boom">${MINE}</td>`;
+                else if (currCell.isMine)
+                    strHTML += ` class="shown">${MINE}</td>`;
+                else if (currCell.minesAroundCount > 0)
+                    strHTML += ` class="shown">${currCell.minesAroundCount}</td>`;
+                else strHTML += ` class="shown"></td>`;
+            }
         }
-        strHTML += '</td>'
+        
 
     }
     strHTML += '</tr>';
@@ -53,3 +77,99 @@ function renderBoard(board) {
     elboard.innerHTML = strHTML;
 }
 
+function cellClicked(i, j) {
+    if (!gGame.isOn)
+        return;
+    if (gBoardMatrix[i][j].isMine || gBoardMatrix[i][j].isMarked || gBoardMatrix[i][j].isShown)
+        return;
+    if (gGame.firstMove) {
+        var minesArray = setMines(i, j);
+        gGame.firstMove = false;
+    }
+
+    if (gBoardMatrix[i][j].minesAroundCount === 0) {
+        gGame.shownCount += expand(i, j);
+    }
+    gBoardMatrix[i][j].isShown = true;
+    gGame.shownCount++;
+    renderBoard(gBoardMatrix);
+
+    if (gBoardMatrix[i][j].isMine) {
+        gLevel.lives--;
+        if (gLevel.lives === 0) {
+            gameOver();
+            showMines(minesArray);
+        }
+    }
+}
+
+function showMines(minesArray) {
+    for (var i = 0; i < minesArray.length; i++) {
+        var iPos = minesArray[i].i;
+        var jPos = minesArray[i].j;
+        gBoardMatrix[iPos][jPos].isShown = true;
+    }
+}
+
+function gameOver() {
+    gelSmily.innerText = '😞 GAME OVER';
+    gGame.isOn = false;
+    gGame.lives = 0;
+}
+
+function setMines(cellI, cellJ) {
+    var cellNeighbors = [];
+    var cellMinesPos = [];
+    for (var i = 0; i < gBoardMatrix.length; i++) {
+        for (var j = 0; j < gBoardMatrix[0].length; j++) {
+            if (i === cellI && j === cellJ) continue
+            cellNeighbors.push({ i: i, j: j })
+        }
+    }
+
+    while (gLevel.mines > 0) {
+        var randomPos = getRandomIntInclusive(0, cellNeighbors.length - 1)
+        var iPos = parseInt(cellNeighbors[randomPos].i);
+        var jPos = parseInt(cellNeighbors[randomPos].j);
+        gBoardMatrix[iPos][jPos].isMine = true;
+        cellMinesPos.push({ i: iPos, j: jPos });
+        gLevel.mines--;
+    }
+
+    for (var i = 0; i < gBoardMatrix.length; i++) {
+        for (var j = 0; j < gBoardMatrix[0].length; j++) {
+            gBoardMatrix[i][j].minesAroundCount = cellMinesAround(gBoardMatrix, i, j);
+        }
+    }
+
+    renderBoard(gBoardMatrix);
+
+    return cellMinesPos;
+}
+
+
+function setDifficulty(level) {
+    switch (level) {
+        case 1:
+            gLevel.size = 4;
+            gLevel.mines = 2;
+            gLevel.flags = 2;
+            gLevel.lives = 1;
+            break;
+
+        case 2:
+            gLevel.size = 8;
+            gLevel.mines = 4;
+            gLevel.flags = 4;
+            gLevel.lives = 2;
+            break;
+
+        case 3:
+            gLevel.size = 16;
+            gLevel.mines = 8;
+            gLevel.flags = 8;
+            gLevel.lives = 3;
+            break;
+    }
+    initGame();
+}
--- util.js
+++ util.js
@@ -1,46 +1,60 @@
-function printMat(mat, selector) {
-    var strHTML = '<table border="0"><tbody>';
-    for (var i = 0; i < mat.length; i++) {
-        strHTML += '<tr>';
-        for (var j = 0; j < mat[0].length; j++) {
-            var cell = mat[i][j];
-            var className = 'cell cell' + i + '-' + j;
-            strHTML += '<td class="' + className + '"> ' + cell + ' </td>'
-        }
-        strHTML += '</tr>'
-    }
-    strHTML += '</tbody></table>';
-    var elContainer = document.querySelector();
-    elContainer.innerHTML = strHTML;
-}
-
-// location such as: {i: 2, j: 7}
-function renderCell(location, value) {
-    // Select the elCell and set the value
-    var elCell = document.querySelector(`.cell${location.i}-${location.j}`);
-    elCell.innerHTML = value;
-}
-
 function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
 
- function createMat(gBoard, rows, cols) {
+function createMat(gBoard, rows, cols) {
     var mat = []
     for (var i = 0; i < rows; i++) {
         var row = []
         for (var j = 0; j < cols; j++) {
             row.push(gBoard);
         }
-        mat.push(row)
+        mat.push(row);
     }
-    return mat
+    return mat;
+}
+
+function cellMinesAround(board, cellI, cellJ) {
+    var cellArea = {
+        currentCell: [],
+        minesAround: [],
+        numberedCells: [],
+        emptyCells: []
+    };
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue;
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (j < 0 || j >= board[i].length) continue;
+
+            if (i === cellI && j === cellJ) {
+                cellArea.currentCell.push({ i, j });
+                continue;
+            }
+
+            var currCell = board[i][j];
+            if (currCell.isMine) cellArea.minesAround.push({ i, j });
+            if (!currCell.isShown) {
+                (currCell.minesAroundCount) ? cellArea.numberedCells.push({ i, j }) : cellArea.emptyCells.push({ i, j });
+            }
+        }
+    }
+    return cellArea.minesAround.length;
+}
+
+
+function expand(coordI, coordJ) {
+    var expandCount = 0;
+    for (var i = coordI - 1; i <= coordI + 1; i++) {
+        if (i < 0 || i >= gBoardMatrix.length) continue;
+        for (var j = coordJ - 1; j <= coordJ + 1; j++) {
+            if (j < 0 || j >= gBoardMatrix.length) continue;
+            if (i === coordI && j === coordJ) continue;
+            if (!gBoardMatrix[i][j].isMine && !gBoardMatrix[i][j].isShown && !gBoardMatrix[i][j].isMarked) {
+                gBoardMatrix[i][j].isShown = true;
+                gGame.shownCount++;
+                expandCount++;
+            }
+        }
+    }
+    return expandCount;
 }
